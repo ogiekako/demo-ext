52c79633a2cb5a9873456ff7f25ca4f4ac88d6f3
Keigo Oka <ogiekako@gmail.com>
2023-09-26 05:10:39 +0000

    Add driver

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as driver from './driver';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -17,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from demo-ext in a web extension host!');
+		vscode.window.showInformationMessage(`Hello ${driver.name()}!`);
 	});
 
 	context.subscriptions.push(disposable);
