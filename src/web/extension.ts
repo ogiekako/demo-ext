8e21b48afaf8a8683f216c86d27387627954a518
Keigo Oka <ogiekako@gmail.com>
2023-09-26 05:13:20 +0000

    Tab to spaces

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -7,21 +7,21 @@ import * as driver from './driver';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "demo-ext" is now active in the web extension host!');
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "demo-ext" is now active in the web extension host!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('demo-ext.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('demo-ext.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage(`Hello ${driver.name()}!`);
-	});
+        // Display a message box to the user
+        vscode.window.showInformationMessage(`Hello ${driver.name()}!`);
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
